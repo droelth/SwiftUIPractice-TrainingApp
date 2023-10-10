1e59e782fe8a0a46b4bbd1af407aadf233a85e57
Berkay <berkaysutlu94@gmail.com>
2023-10-10 22:43:52 +0300

    Initial Commit

---
+++ UIP-TrainingApp/UIP_TrainingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIP_TrainingAppApp.swift
+//  UIP-TrainingApp
+//
+//  Created by Berkay Sutlu on 10.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct UIP_TrainingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
